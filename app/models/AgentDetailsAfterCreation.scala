faf7a1cf7e5b536a560a1100e271504bb4fab0d7
DavouJ <96893264+DavouJ@users.noreply.github.com>
2025-11-21 11:55:34 +0000

    DTR-1324: Updating agent details model

--- app/models/AgentDetailsAfterCreation.scala
+++ app/models/AgentDetailsAfterCreation.scala
@@ -19,16 +19,16 @@ package models
 import play.api.libs.json.{Json, OFormat}
 //TODO: update
 case class AgentDetailsAfterCreation(
-                                       agentName             : String,
+                                       storn                 : String,
                                        name                  : String,
-                                       houseNumber           : String,
-                                       addressLine1          : String,
+                                       houseNumber           : Option[String],
+                                       addressLine1          : Option[String],
                                        addressLine2          : Option[String],
-                                       addressLine3          : String,
+                                       addressLine3          : Option[String],
                                        addressLine4          : Option[String],
                                        postcode              : Option[String],
                                        phone                 : Option[String],
-                                       email                 : String
+                                       email                 : Option[String]
                                     )
 
 object AgentDetailsAfterCreation {
