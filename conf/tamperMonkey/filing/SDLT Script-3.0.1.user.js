9345158bc6799ddc0b449cd4c63f896e1caabae9
dilli.mateti <238047753+dillimateti501@users.noreply.github.com>
2026-02-12 16:57:35 +0000

    added 8999 and 8998

--- conf/tamperMonkey/filing/SDLT Script-3.0.1.user.js
+++ conf/tamperMonkey/filing/SDLT Script-3.0.1.user.js
@@ -60,7 +60,9 @@ var userProfiles = '[' +
     '{ "description":"purchaser with land transaction with scott authcodes", "enrolmentKey":"IR-SDLT-ORG", "identifierName":"STORN", "identifierValue":"", "enrolmentStatus":"Activated", "affinityGroup":"Organisation", "returnId": "full-purchaser-with-agent-land-transaction-with-scot-authcodes" }, ' +
     '{ "description":"purchaser with land transaction with empty eff and contract dates", "enrolmentKey":"IR-SDLT-ORG", "identifierName":"STORN", "identifierValue":"", "enrolmentStatus":"Activated", "affinityGroup":"Organisation", "returnId": "full-purchaser-with-agent-land-transaction-emptydates" }, ' +
     '{ "description":"purchaser with land transaction with uk authcode with scot postcode", "enrolmentKey":"IR-SDLT-ORG", "identifierName":"STORN", "identifierValue":"", "enrolmentStatus":"Activated", "affinityGroup":"Organisation", "returnId": "full-purchaser-with-agent-land-transaction-ukauthcode-with-scot-postcode" }, ' +
-    '{ "description":"purchaser with land transaction with welsh authcodes", "enrolmentKey":"IR-SDLT-ORG", "identifierName":"STORN", "identifierValue":"", "enrolmentStatus":"Activated", "affinityGroup":"Organisation", "returnId": "full-purchaser-with-agent-land-transaction-welsh-authcodes" } ' +
+    '{ "description":"purchaser with land transaction with welsh authcodes", "enrolmentKey":"IR-SDLT-ORG", "identifierName":"STORN", "identifierValue":"", "enrolmentStatus":"Activated", "affinityGroup":"Organisation", "returnId": "full-purchaser-with-agent-land-transaction-welsh-authcodes" }, ' +
+    '{ "description":"purchaser with land transaction 8998", "enrolmentKey":"IR-SDLT-ORG", "identifierName":"STORN", "identifierValue":"", "enrolmentStatus":"Activated", "affinityGroup":"Organisation", "returnId": "full-purchaser-with-agent-land-transaction-8998" }, ' +
+    '{ "description":"purchaser with land transaction 8999", "enrolmentKey":"IR-SDLT-ORG", "identifierName":"STORN", "identifierValue":"", "enrolmentStatus":"Activated", "affinityGroup":"Organisation", "returnId": "full-purchaser-with-agent-land-transaction-8999" } ' +
     ']';
 
 
