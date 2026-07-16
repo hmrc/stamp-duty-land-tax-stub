e8f1068440237b996d2235b237d3219aed1f2d53
glennsoft2 <241766806+glennsoft2@users.noreply.github.com>
2026-07-16 09:25:11 +0100

    DTR-5390 email service stubs

---
+++ app/uk/gov/hmrc/stampdutylandtaxstub/controllers/EmailController.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.stampdutylandtaxstub.controllers
+
+import models.requests.EmailServiceRequest
+import play.api.Logging
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc.{Action, ControllerComponents}
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+import uk.gov.hmrc.stampdutylandtaxstub.util.StubResource
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton()
+class EmailController @Inject()(cc: ControllerComponents, override val executionContext: ExecutionContext)
+  extends BackendController(cc) with StubResource with Logging:
+
+  def sendEmailWithRequest: Action[JsValue] = Action.async(parse.json) { implicit request =>
+    request.body.validate[EmailServiceRequest].fold(
+      invalid => Future.successful(BadRequest(Json.obj("message" -> s"Invalid payload: $invalid"))),
+      response => {
+        val fullPath = s"/resources.data.email/emailServiceRequest.json"
+
+        findResource(fullPath) match {
+          case Some(content) => Future.successful(jsonResourceAsResponse(fullPath)) 
+          case _ => Future.successful(NotFound)
+        }
+      }
+    )
+  }
